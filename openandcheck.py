742045c634857042ef65d70d1a25d2b6b248f005
Kyle <kam3634@rit.edu>
2024-05-05 20:04:09 -0400

    6-variable array

--- openandcheck.py
+++ openandcheck.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import random
 
 
 def check(number):
@@ -8,17 +9,20 @@ def check(number):
     with open (path, 'r') as file:
         data = json.load(file)
     filename = data['target']['file']['name']
-    httpreqs = len(data['network']['http'])
-    dnsreqs = len(data['network']['dns'])
-    print(data['network']['dns'])
-    print(dnsreqs)
-    print(httpreqs)
-    return filename
+    httpreqs = len(data['network']['http']) #HTTP requests
+    dnsreqs = len(data['network']['dns']) #DNS connections
+    tcpreqs = len(data['network']['tcp']) #TCP connections
+    domains = len(data['network']['domains']) #domains reached
+    behaviors = len(data['behavior']['generic']) #Behaviors Observed
+    processes = len(data['behavior']['processtree']) #Processes Spawned
+    print(data['behavior']['processtree'])
+    returner = [httpreqs, dnsreqs, tcpreqs, domains, processes, behaviors]
+    return returner
 
 
 
 def main():
     number = input("Pick a number 11-405: ")
-    print (check(number))
+    print (check(str(number)))
 
 main()
